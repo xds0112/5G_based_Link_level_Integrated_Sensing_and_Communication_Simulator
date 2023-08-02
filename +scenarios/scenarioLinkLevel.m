fa0a2c38cb5e725004c08f177aff18dc1d2be4b6
Dongsheng Xue <xuedongsheng@bupt.edu.cn>
2023-08-02 11:32:02 +0800

    Update scenarioLinkLevel.m

--- +scenarios/scenarioLinkLevel.m
+++ +scenarios/scenarioLinkLevel.m
@@ -17,15 +17,16 @@ function simuParams = scenarioLinkLevel()
     % Initialize bs
     bs = networkElements.baseStation.gNB;
     bs.ID               = 1;
-    bs.position         = [0 0 10];        % in meters
-    bs.txPower          = 46;              % in dBm
-    bs.carrierFrequency = 28.50e9;         % in Hz
-    bs.bandwidth        = 100;             % in MHz
-    bs.scs              = 60;              % in kHz
-    bs.tddPattern       = 'D';             % full downlink
-    bs.specialSlot      = [14 0 0];        % full downlink symbols
-    bs.antSize          = [4 1 2];         % BS antenna panel
-    bs.cfarEstZone      = [50 500; -20 20];
+    bs.position         = [0 0 10];         % in meters
+    bs.txPower          = 46;               % in dBm
+    bs.carrierFrequency = 28.50e9;          % in Hz
+    bs.bandwidth        = 100;              % in MHz
+    bs.scs              = 60;               % in kHz
+    bs.tddPattern       = 'D';              % full downlink
+    bs.specialSlot      = [14 0 0];         % full downlink symbols
+    bs.antSize          = [4 1 2];          % BS antenna panel
+    bs.Pfa              = 1e-9;             % false alarm rate
+    bs.cfarEstZone      = [50 500; -20 20]; % [a b; c d], a to b m, c to d m/s
     
     % Attached UEs
     ue = networkElements.ue.basicUE();
@@ -34,19 +35,12 @@ function simuParams = scenarioLinkLevel()
     ueParams    = ue;
     
     % Attached targets
-    tgt(1) = networkElements.target.basicTarget();
-    tgt(1).ID       = 1;
-    tgt(1).position = [100 100 1.5];  % in meters
-    tgt(1).rcs      = 1;              % in meters^2
-    tgt(1).velocity = 10;             % in meters per second
-
-    tgt(2) = networkElements.target.basicTarget();
-    tgt(2).ID       = 2;
-    tgt(2).position = [150 100 1.5];  % in meters
-    tgt(2).rcs      = 1;              % in meters^2
-    tgt(2).velocity = -5;             % in meters per second
-
-    tgtParams       = tgt;
+    tgt = networkElements.target.basicTarget();
+    tgt.ID       = 1;
+    tgt.position = [100 100 1.5];  % in meters
+    tgt.rcs      = 1;              % in meters^2
+    tgt.velocity = 5;              % in meters per second
+    tgtParams    = tgt;
     
     % Update bs and UE/target pairs
     bs.attachedUEs  = ueParams;
