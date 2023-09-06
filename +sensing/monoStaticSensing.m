b00af70bb631ee91a3416fc8407bdfeaa2537e0b
Dongsheng Xue <xuedongsheng@bupt.edu.cn>
2023-09-06 17:17:26 +0800

    Update monoStaticSensing.m

--- +sensing/monoStaticSensing.m
+++ +sensing/monoStaticSensing.m
@@ -1,15 +1,12 @@
-function echoGrid = monoStaticSensing(txGrid, carrier, waveInfo, bsParams, rdrEstParams, topoParams)
+function echoGrid = monoStaticSensing(txWaveform, carrier, waveInfo, bsParams, rdrEstParams, topoParams)
 % Simulate gNB-based mono-static sensing.
 
 % Author: D.S.Xue, Key Laboratory of Universal Wireless Communications,
 % Ministry of Education, BUPT.
 %%
-    % OFDM modulation
-    txWaveform = nrOFDMModulate(carrier, txGrid);
-
-    % Calculate the amplitude of the transmitted signal
+    % Apply large-scale gain
     nTxAnts = prod(bsParams.antConfig.bsAntSize);
-    sigAmp = db2mag(bsParams.txPower-30)*sqrt(waveInfo.Nfft^2/(carrier.NSizeGrid*12*nTxAnts));
+    sigAmp  = db2mag(bsParams.txPower-30)*sqrt(waveInfo.Nfft^2/(carrier.NSizeGrid*12*nTxAnts));
     txWaveform = sigAmp*txWaveform;
 
     % Apply radar propagation channel
