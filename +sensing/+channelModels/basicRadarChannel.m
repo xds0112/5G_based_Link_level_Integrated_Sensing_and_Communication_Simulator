d247427fa0c185e2838cdcad133780fc9eea43db
Dongsheng Xue <xuedongsheng@bupt.edu.cn>
2023-09-08 17:13:24 +0800

    Update basicRadarChannel.m

--- +sensing/+channelModels/basicRadarChannel.m
+++ +sensing/+channelModels/basicRadarChannel.m
@@ -1,6 +1,6 @@
-function rxWaveform = basicRadarChannel(txWaveform, waveInfo, simParams, radarEstParams, topoParams)
-% Simulate multi-target OFDM radar propagation channel.
-
+function rxWaveform = basicRadarChannel(txWaveform, radarEstParams)
+% Simulate time-domain multi-target OFDM radar propagation channel.
+%
 % Author: D.S Xue, Key Laboratory of Universal Wireless Communications,
 % Ministry of Education, BUPT.
 
@@ -9,31 +9,33 @@ function rxWaveform = basicRadarChannel(txWaveform, waveInfo, simParams, radarEs
 
     % OFDM parameters
     c      = physconst('Lightspeed');
-    fc     = simParams.carrierFrequency;     % carrier frequency
-    lambda = c/fc;                           % wave length
-    fs     = waveInfo.SampleRate;            % sampling rate/symbol rate
-    Ts     = 1/fs;                           % sampling-time interval
+    fc     = radarEstParams.fc;     % carrier frequency
+    lambda = c/fc;                  % wavelength
+    fs     = radarEstParams.fs;     % sampling rate/symbol rate
+    Ts     = 1/fs;                  % sampling-time interval
 
     %% Sensing Parameters
-    nTargets = numel(simParams.attachedTgts);
-
     % Range parameters
-    pathDelay = 2*topoParams.rangeTgts.'/c; % echo delay
-    symbolShift = ceil(pathDelay./Ts);      % echo symbol shift
+    pathDelay = 2*radarEstParams.rng.'/c; % echo delay
+    symbolShift = ceil(pathDelay./Ts);    % echo symbol shift
 
     % Velocity parameters
-    fd = 2*topoParams.velocityTgts./lambda; % echo doppler shift
+    fd = 2*radarEstParams.vel./lambda; % echo doppler shift
+
+    % Number of targets
+    nTargets = numel(radarEstParams.rng);
 
     %% Multi-Target Radar Propagation Channel
     % Send Tx signal
-    sampleTimeTx = (0:Ts:Ts*(txWaveLength-1)).';        % transpose to make a column vector
-    phaseTx      = exp(2j*pi*fc*sampleTimeTx);          % carrier corresponding to Rx [txWaveLength x nTxAnts]
-    txWaveform   = bsxfun(@times, txWaveform, phaseTx); % modulation [txWaveLength x nTxAnts]
+    sampleTimeTx = (0:Ts:Ts*(txWaveLength-1)).';
+    phaseTx      = exp(2j*pi*fc*sampleTimeTx);          % carrier corresponding to Tx, [txWaveLength x nTxAnts]
+    txWaveform   = bsxfun(@times, txWaveform, phaseTx); % modulation, [txWaveLength x nTxAnts]
 
     % Simulate small-scale propagation channel
     largeScaleFading = radarEstParams.lsFading;
-    RxSteeringVector = radarEstParams.RxSteeringVec; % [nTxAnts x nTargets]
-    TxSteeringVector = RxSteeringVector; % Tx and Rx shares the same steering vector
+    txSteeringVector = radarEstParams.txSteeringVector; % Tx steering vector, [nTxAnts x nTargets]
+    rxSteeringVector = radarEstParams.rxSteeringVector; % Rx steering vector, [nRxAnts x nTargets]
+    nRxAnts          = size(rxSteeringVector, 1);
 
     % Parameters initialization
     rxWaveLength = zeros(1, nTargets);
@@ -41,26 +43,30 @@ function rxWaveform = basicRadarChannel(txWaveform, waveInfo, simParams, radarEs
 
     for i = 1:nTargets
         % Apply echo path delay and doppler effect
-        rxEchoWave{i}   = [zeros(symbolShift(i), nTxAnts); txWaveform(1:end-symbolShift(i),:)];
-        sampleTimeCh{i} = (0:Ts:Ts*(size(rxEchoWave{i}, 1)-1)).';       % column vector of sampling times
-        phaseShift{i}   = exp(2j*pi*fd(i)*sampleTimeCh{i});             % phase shift caused by Doppler effect
-        rxEchoWave{i}   = bsxfun(@times, rxEchoWave{i}, phaseShift{i}); % apply phase shift
+        rxEchoWave{i} = [zeros(symbolShift(i), nTxAnts); txWaveform(1:end-symbolShift(i),:)];
+
+        % Column vector of sampling times
+        sampleTimeCh{i} = (0:Ts:Ts*(size(rxEchoWave{i}, 1)-1)).';
+
+        % Apply phase shift caused by Doppler effect
+        phaseShift{i} = exp(2j*pi*fd(i)*sampleTimeCh{i});
+        rxEchoWave{i} = bsxfun(@times, rxEchoWave{i}, phaseShift{i});
 
         % Apply large-scale fading
         rxEchoWave{i} = bsxfun(@times, rxEchoWave{i}, largeScaleFading(i));
 
         % Apply angle steering vector
-        rxEchoWave{i} = rxEchoWave{i}*RxSteeringVector(:,i)*TxSteeringVector(:,i).';
+        rxEchoWave{i} = rxEchoWave{i}*txSteeringVector(:,i)*rxSteeringVector(:,i).';
 
         % Append zero padding
         rxWaveLength(i) = size(rxEchoWave{i}, 1);
         if rxWaveLength(i) < txWaveLength
-            rxEchoWave{i} = [rxEchoWave{i}; zeros(txWaveLength-rxWaveLength(i), nTxAnts)];
+            rxEchoWave{i} = [rxEchoWave{i}; zeros(txWaveLength-rxWaveLength(i), nRxAnts)];
         end
     end
 
     % Combine echoes
-    rxWaveform = sum(cat(3,rxEchoWave{:}), 3); % [rxWaveLength x nRxAnts]
+    rxWaveform = sum(cat(3, rxEchoWave{:}), 3); % [rxWaveLength x nRxAnts]
 
     % Apply AWGN
     N0         = sqrt(radarEstParams.N0/2.0);
@@ -68,7 +74,7 @@ function rxWaveform = basicRadarChannel(txWaveform, waveInfo, simParams, radarEs
     rxWaveform = rxWaveform + noise;
 
     % Receive base-band signal
-    sampleTimeRx = (0:Ts:Ts*(size(rxWaveform, 1)-1)).';  % transpose to make a column vector
+    sampleTimeRx = (0:Ts:Ts*(size(rxWaveform, 1)-1)).';
     phaseRx      = exp(-2j*pi*fc*sampleTimeRx);          % carrier corresponding to Rx [rxWaveLength x 1]
     rxWaveform   = bsxfun(@times, rxWaveform, phaseRx);  % base-band signal [rxWaveLength x nRxAnts]
 
