12eca378d65138d10bcffa4a61582b5a1ab621da
Dongsheng Xue <xuedongsheng@bupt.edu.cn>
2023-09-08 17:27:31 +0800

    Update scenarioFullDownlink.m

--- +scenarios/scenarioFullDownlink.m
+++ +scenarios/scenarioFullDownlink.m
@@ -23,7 +23,9 @@ function simuParams = scenarioFullDownlink()
     bs.scs              = 60;               % in kHz
     bs.tddPattern       = "D";              % full downlink
     bs.specialSlot      = [14 0 0];         % full downlink symbols
-    bs.antSize          = [8 1 1];          % antenna panel
+    bs.txAntSize        = [8 8 1];          % transmission antenna panel
+    bs.rxComAntSize     = [8 4 1];          % reception antenna panel for communication
+    bs.rxSenAntSize     = [8 4 1];          % reception antenna panel for sensing
     bs.Pfa              = 1e-9;             % false alarm rate
     bs.cfarEstZone      = [50 500; -20 20]; % [a b; c d], a to b m, c to d m/s
     
